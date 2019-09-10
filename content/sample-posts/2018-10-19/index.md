b367daa80568b35946492ed6977ffd73b8e9ac42
Bobby Wibowo <bobbywibowo98@gmail.com>
2019-09-11 04:22:29 +0700

    Initial commit from gatsby: (https://github.com/GatsbyCentral/gatsby-v2-starter-casper.git)

---
+++ content/sample-posts/2018-10-19/index.md
@@ -0,0 +1,30 @@
+---
+title: "Upgraded to Gatsby v2"
+cover: "https://unsplash.it/1280/900/?random?TheFallenTime"
+author: "casper"
+date: "2018-10-19"
+category: "Gatsby"
+tags:
+    - gatsby
+    - v2
+---
+This starter has been forked and upgraded to Gatsby v2.
+
+This is a Gatsby v2 fork of [gatsby-starter-casper](https://github.com/haysclark/gatsby-starter-casper) by [@haysclark](https://github.com/haysclark). The upgrade to v2 was mostly done by [@hnspn](https://github.com/hnspn) with a little help from the folks at [GatsbyCentral](https://www.gatsbycentral.com/).
+
+Here's a little recommended reading for Gatsby v2.
+
+* First, [what's new in v2](https://www.gatsbycentral.com/whats-new-in-gatsby-v2)
+* Next, [when should I upgrade?](https://www.gatsbycentral.com/when-should-i-upgrade-to-gatsby-v2)
+* [The Gatsby v2 announcement blog post](https://www.gatsbyjs.org/blog/2018-09-17-gatsby-v2/)
+
+If you **do decide to upgrade**, these articles will help you do that.
+
+* [Getting started with Gatsby v2](https://www.gatsbycentral.com/getting-started-with-gatsby-v2)
+* [Should I rebuild my site for Gatsby v2?](https://www.gatsbycentral.com/should-i-rebuild-my-site-for-gatsby-v2)
+* [How do layouts work in Gatsby v2](https://www.gatsbycentral.com/how-do-layouts-work-in-gatsby-v2)
+* [StaticQuery in Gatsby v2](https://www.gatsbycentral.com/staticquery-in-gatsby-v2)
+
+Finally, here's a cheat sheet we prepared to help during the upgrade:
+
+* [Gatsby v2 refactoring cheat sheet](https://www.gatsbycentral.com/gatsby-v2-refactoring-cheat-sheet)
---
+++ data/SiteConfig.js
@@ -0,0 +1,55 @@
+module.exports = {
+  blogPostDir: "sample-posts", // The name of directory that contains your posts.
+  blogAuthorDir: "sample-authors", // The name of directory that contains your 'authors' folder.
+  blogAuthorId: "casper", // The default and fallback author ID used for blog posts without a defined author.
+  siteTitle: "Gatsby v2 Casper Starter", // Site title.
+  siteTitleAlt: "GatsbyJS Casper Theme Starter", // Alternative site title for SEO.
+  siteLogo:
+    "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
+  siteUrl: "https://haysclark.github.io", // Domain of your website without pathPrefix.
+  pathPrefix: "/gatsby-starter-casper", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
+  siteDescription:
+    "A Casper themed GatsbyJS stater based on the Advanced Starter.", // Website description used for RSS feeds/meta description tag.
+  siteCover:
+    "https://haysclark.github.io/gatsby-starter-casper/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
+  siteNavigation: true, // If navigation is enabled the Menu button will be visible
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteRssAuthor: "Casper User", // The author name used in the RSS file
+  // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
+  sitePaginationLimit: 10, // The max number of posts per page.
+  googleAnalyticsID: "UA-111982167-1", // GA tracking ID.
+  // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
+  siteSocialUrls: [
+    "https://github.com/haysclark/gatsby-starter-casper",
+    "https://twitter.com/gatsbyjs",
+    "mailto:gatsbyjs@example.com"
+  ],
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  // Links to social profiles/projects you want to display in the navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/haysclark/gatsby-starter-casper",
+      iconClassName: "fa fa-github" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/gatsbyjs",
+      iconClassName: "fa fa-twitter" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "Email",
+      url: "mailto:gatsbyjs@example.com",
+      iconClassName: "fa fa-envelope" // Disabled, see Navigation.jsx
+    }
+  ],
+  // Copyright string for the footer of the website and RSS feed.
+  copyright: {
+    label: "Gatsby Casper Starter" // Label used before the year
+    // year: "2018" // optional, set specific copyright year or range of years, defaults to current year
+    // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
+  },
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  promoteGatsby: true // Enables the GatsbyJS promotion information in footer.
+};
