3980adccc80d5992c94989bdfdc625a346f740a8
Bobby Wibowo <bobbywibowo98@gmail.com>
2020-09-04 14:20:52 +0700

    Updated gastby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ require('prismjs/themes/prism-tomorrow.css')
 
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    'This application has been updated. Reload to display the latest version?'
+    'This blog\'s client assets have recently been updated. Reload to display the latest version?'
   )
   if (answer === true)
     window.location.reload()
